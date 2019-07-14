8c044d29d930cea265593926a7c3fac7946cfbac
Edward GC <rgdesarrollo@gmail.com>
2019-07-14 14:02:56 +0200

    Temple Scroll page

---
+++ main.js
@@ -0,0 +1,16 @@
+// option 2  - jQury Smooth Scoll 
+
+$('.navbar a').on('click ', function(e){
+    if(this.hash !== '') {
+        e.preventDefault();
+
+        const hash = this.hash;
+        
+        $('html, body').animate({
+            scrollTop: $(hash).offset().top
+        },800
+        );
+
+
+    }
+});
